2458be6b329d96c57992c1173d204a85a1342cbc
Chanelle Ractliffe <chanelle.ractliffe@gmail.com>
2018-03-19 18:48:06 -0400

    problem solved

---
+++ Simple_array_sum.py
@@ -0,0 +1,15 @@
+#
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(n, ar):
+    "" integer simpleArraySum(integer n, integer_array ar) {
+    # Return the sum of all array elements
+}
+n:  Integer denoting number of array elements
+ar: Integer array with elements whose sum needs to be computed
+    ""
+    return sum(ar)
+
+
+# this problem took me a whole 5 minutes because it looked harder
+# than it was! no need for n, simply use sum of your list
